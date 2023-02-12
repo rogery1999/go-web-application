85386c5cc08b60ac14b3aa8ebc90779eabe93ebb
Rogery Vargas <rogery1999@hotmail.com>
2023-02-11 23:37:10 -0500

    feat: gin package added

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Salute struct {
+	Message string `json:"message"`
+}
 
 func main() {
 	fmt.Println("Hello There!")
+
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		log.Println("New Change")
+		ctx.JSON(http.StatusOK, Salute{Message: "Hello World!"})
+	})
+
+	r.Run("127.0.0.1:8000")
 }
